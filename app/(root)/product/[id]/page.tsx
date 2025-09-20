bf536564b0e515c1b709679739980e7ffcaf7a90
Shkypperrr <justalex20069@gmail.com>
2025-09-20 12:30:24 +0300

    Fix ProductPage with PageProps

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -1,15 +1,15 @@
 import { ProductForm, ProductImage } from '@/components/shared'
-import { Container } from '@/components/shared/container'
+
 import { prisma } from '@/prisma/prisma-client'
 import { notFound } from 'next/navigation'
 
-interface ProductPageProps {
-	params: { id: string }
+type PageProps = {
+	params: {
+		id: string
+	}
 }
 
-export default async function ProductPage({
-	params: { id },
-}: ProductPageProps) {
+export default async function ProductPage({ params: { id } }: PageProps) {
 	const product = await prisma.product.findFirst({
 		where: { id: Number(id) },
 		include: {
